bbb5c8ad9f833262f86068a2134f40d9927e052d
mtrusca24 <truscamarius@gmail.com>
2019-01-16 00:16:51 +0200

    First commit

---
+++ src/main/java/ex1/Eu.java
@@ -0,0 +1,26 @@
+package ex1;
+
+public class Eu {
+
+	public static void main(String[] args) {
+		System.out.println("eu merg la mare");
+		/*
+		 * git init
+		 * 
+		 * git add .
+		 * 
+		 * git commit -m "First commit"
+		 * 
+		 * git remote add origin https://github.com/mtrusca24/gitandgithub2.git
+# Sets the new remote
+git remote -v
+# Verifies the new remote URL
+
+git push origin master
+		 * 
+		 * 
+		 */
+
+	}
+
+}
